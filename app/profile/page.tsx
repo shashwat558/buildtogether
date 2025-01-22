90b17af99740e3178f61c68a2f2ba7575a86b9f5
shashwat558 <jainshashwat528@gmail.com>
2025-01-22 23:44:48 +0530

    Enhance profile page with user details display and update user retrieval logic

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -6,14 +6,21 @@ import { getUserDetails } from '@/lib/actions';
 const page = async() => {
     const session = await auth();
     const email = session?.user?.email as string;
-    const userInfo = getUserDetails(email)
+    const userInfo = await getUserDetails(email)
     if(!session){
         redirect("/")
     }
     console.log(JSON.stringify(userInfo))
-    console.log(email)
+    console.log((email))
   return (
-    <div>page</div>
+    <div className='w-1/2 h-[800px] border-2  rounded-md p-5 shadow-md bg-gray-900 '>
+      <div className='w-10 h-10 rounded-full bg-white text-center text-2xl'>{userInfo?.name?.split("")[0]}</div>
+      <h1 className='text-white'>{userInfo?.name}</h1>
+
+
+
+
+    </div>
   )
 }
 
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,11 +1,13 @@
 import { prisma } from "./prisma"
 
 export const getUserDetails = async (userEmail: string) => {
-    const user = await prisma.user.findUnique({
+    const user = await prisma.user.findFirst({
         where:{
             email: userEmail
-        }
+        },
+        
     });
+    
     return user;
 }
 
