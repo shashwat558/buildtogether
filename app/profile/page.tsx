83fc970135bd22cf97cf4ad66e20d829b623f8da
shashwat558 <jainshashwat528@gmail.com>
2025-02-02 23:47:42 +0530

    Enhance profile page styling and layout with Poppins font and improved user details display

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import React, { useEffect, useState } from 'react'
-
+import {  Poppins } from 'next/font/google';
 
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
+import { School } from 'lucide-react';
 
 
+const Poppin = Poppins({
+weight: "400",
+subsets: ["latin"]
+})
 interface UserDetailsType {
   username: string,
   college: {
@@ -54,11 +59,19 @@ const Page = () => {
     }, [session])
     
   return (
-    <div className='w-1/2 h-[800px] border-2  rounded-md p-5 shadow-md bg-gray-900 '>
-      <div className='w-40 h-40 rounded-full overflow-hidden bg-white text-center text-2xl flex justify-center items-center'>
+    <div className={`${Poppin.className} w-1/2 h-[800px] rounded-md p-5 shadow-md `}>
+
+      <div className='flex items-center gap-x-6'>
+
+      <div className='w-40 h-40 rounded-full overflow-hidden bg-transparent text-center text-2xl flex justify-center items-center'>
         {userDetails?.profileImage ? <Image className="object-fill"  src={userDetails?.profileImage?? userDetails?.username.split(""[0])} alt='profilePic' width={200} height={200}/>: <div>{userDetails?.username.split("")[0]}</div>}
       </div>
-      <h1 className='text-white'>{userDetails?.username}</h1>
+      <div className='flex flex-col gap-5 justify-center'>
+      <h1 className='text-5xl text-white'><span className='text-xl underline mr-2'>username:</span>{userDetails?.username}</h1>
+      <h1 className='text-4xl text-white'><span><School /></span> {userDetails?.college.name}</h1>
+      </div>
+      
+      </div>
 
 
 
