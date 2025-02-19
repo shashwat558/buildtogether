77e440e60bf245e95f6b25f5cb5b302710d74b13
shashwat558 <jainshashwat528@gmail.com>
2025-02-19 18:55:16 +0530

    add college page and update search user routing to use college name

--- components/SearchUser.tsx
+++ components/SearchUser.tsx
@@ -116,7 +116,7 @@ export default function SearchUser() {
           onChange={(e) => setSearchCollegeParams(e.target.value)}
           isLoading={isLoadingColleges}
           results={colleges}
-          onResultClick={(item) => router.push(`/college/${item.id}`)}
+          onResultClick={(item) => router.push(`/college/${item.name}`)}
           renderResult={(college) => college.name}
         />
       </div>
