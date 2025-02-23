82f4c7b161e728a3dba8c78a2a595a89b6ea47bf
shashwat558 <jainshashwat528@gmail.com>
2025-02-23 16:51:15 +0530

    added authentication in chat

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -7,6 +7,11 @@ import {  NextResponse } from "next/server";
 export async function GET(){
 
     const session = await auth()
+    if(!session){
+        return NextResponse.json({message: "got an error"})
+    }
+
+    console.log(session)
 
    
     
